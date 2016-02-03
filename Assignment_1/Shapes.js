70b9bffdaf13dbef528c628cbf2d3180306b4767
Colin Fenton <w0274178@nscc.ca>
2016-02-03 09:39:43 -0400

    triangle and rectangle
    finished rect and tri

--- Assignment_1/Shapes.js
+++ Assignment_1/Shapes.js
@@ -22,14 +22,13 @@ function Square(size)
     Shape.call(this);
     this.size = size;
 }
-//count the number of shapes
+//count the number of squares
 Square.prototype.countSquares = (function () {
     var squares = 0;
     return function () {return squares += 1;}
 })();
-//override - calculates the area of a square
+//override of area calculation
 Square.prototype.area = function() {return Math.pow(this.size, 2);};
-
 //function that creates a square, increments counters, and updates the html
 function addSquare()
 {
@@ -61,32 +60,117 @@ function addSquare()
     squareArea.innerHTML += "Area: " + mySquare.area() + "<br/>";
 }
 
+////////////////
+///Rectangle///
+//////////////
 
-//subclass Rectangle
+//constructor
 function Rectangle(length, width)
 {
     Shape.call(this);
     this.length = length;
     this.width = width;
 }
-//override
+//count the number of rectangles
+Rectangle.prototype.countRectangles = (function () {
+    var rectangles = 0;
+    return function () {return rectangles += 1;}
+})();
+//override of area calculation
 Rectangle.prototype.area = function() {return this.length * this.width;};
+//function that creates a rectangle, increments counters, and updates the html
+function addRectangle()
+{
+    //get the html elements that need to be changed
+    var rectangleCounter = document.getElementById("rectangleCount");
+    var totalCounter = document.getElementById("totalCount");
+    var rectangleArea = document.getElementById("rectangle");
+    var rectangleButton = document.getElementById("rectangleButton");
 
-//subclass Triangle
+    //get the user input for size
+    var lengthInput = document.getElementById("rectangleLength").value;
+    var widthInput = document.getElementById("rectangleWidth").value;
+
+    //create a rectangle with the length and width input
+    var myRectangle = new Rectangle(lengthInput, widthInput);
+
+    //check how many rectangles there are now
+    var numRectangles = myRectangle.countRectangles();
+    //if the number of rectangles is 10, disable the add square button
+    if (numRectangles == 10)
+    {
+        rectangleButton.disabled = true;
+    }
+
+    //update the html with new information
+    rectangleCounter.innerHTML = "Rectangle Count: " + numRectangles;
+    totalCounter.innerHTML = "Total Shapes: " + countTotal();
+    rectangleArea.innerHTML += "</br>" + "Rectangle #"+ numRectangles +"<br/>";
+    rectangleArea.innerHTML += "Length: " + myRectangle.length+"<br/>";
+    rectangleArea.innerHTML += "Width: "+  myRectangle.width+"<br/>";
+    rectangleArea.innerHTML += "Area: " + myRectangle.area() + "<br/>";
+}
+
+///////////////
+///Triangle///
+/////////////
+
+//constructor
 function Triangle(base, height)
 {
     Shape.call(this);
     this.base = base;
     this.height = height;
 }
-//override
+//override of area calculation
 Triangle.prototype.area = function() {return (this.base * this.height)/2;};
+//count the number of triangles
+Triangle.prototype.countTriangles = (function () {
+    var triangles = 0;
+    return function () {return triangles += 1;}
+})();
+//function that creates a triangle, increments counters, and updates the html
+function addTriangle()
+{
+    //get the html elements that need to be changed
+    var triangleCounter = document.getElementById("triangleCount");
+    var totalCounter = document.getElementById("totalCount");
+    var triangleArea = document.getElementById("triangle");
+    var triangleButton = document.getElementById("triangleButton");
 
-//subclass Circle
+    //get the user input for size
+    var baseInput = document.getElementById("triangleBase").value;
+    var heightInput = document.getElementById("triangleHeight").value;
+
+    //create a triangle with the base and height input
+    var myTriangle= new Triangle(baseInput, heightInput);
+
+    //check how many triangles there are now
+    var numTriangles = myTriangle.countTriangles();
+    //if the number of squares is 10, disable the add square button
+    if (numTriangles == 10)
+    {
+        triangleButton.disabled = true;
+    }
+
+    //update the html with new information
+    triangleCounter.innerHTML = "Triangle Count: " + numTriangles;
+    totalCounter.innerHTML = "Total Shapes: " + countTotal();
+    triangleArea.innerHTML += "</br>" + "Triangle #"+ numTriangles +"<br/>";
+    triangleArea.innerHTML += "Base: " + myTriangle.base+"<br/>";
+    triangleArea.innerHTML += "Height: "+  myTriangle.height+"<br/>";
+    triangleArea.innerHTML += "Area: " + myTriangle.area() + "<br/>";
+}
+
+/////////////
+///Circle///
+///////////
+
+//constructor
 function Circle(radius)
 {
     Shape.call(this);
     this.radius = radius;
 }
-//override
+//override of area calculation
 Circle.prototype.area = function() {return Math.PI*(Math.pow((this.radius), 2));};
